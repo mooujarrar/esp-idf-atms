d7df0e535e61bc8a0bc0e9210f7b77c875a4b7f9
mooujarrar <mohyiddine1997@gmail.com>
2023-05-23 20:03:51 +0200

    maint; Add support for rc522 rfid reader

--- main/main.c
+++ main/main.c
@@ -1,18 +1,35 @@
-#include <stdio.h>
-#include "driver/gpio.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
 
-void app_main(void)
+#include <esp_log.h>
+#include <inttypes.h>
+#include "rc522.h"
+
+static const char* TAG = "rc522-demo";
+static rc522_handle_t scanner;
+
+static void rc522_handler(void* arg, esp_event_base_t base, int32_t event_id, void* event_data)
 {
-    uint64_t output_pin_bit_mask = (1ULL << GPIO_NUM_2);
-    gpio_config_t output_pin_config = { output_pin_bit_mask, GPIO_MODE_OUTPUT};
-    if (gpio_config(&output_pin_config) == ESP_OK) {
-        while(true) {
-            gpio_set_level(GPIO_NUM_2, 1);
-            vTaskDelay(500 / portTICK_PERIOD_MS);
-            gpio_set_level(GPIO_NUM_2, 0);
-            vTaskDelay(500 / portTICK_PERIOD_MS);
-        }
+    rc522_event_data_t* data = (rc522_event_data_t*) event_data;
+
+    switch(event_id) {
+        case RC522_EVENT_TAG_SCANNED: {
+                rc522_tag_t* tag = (rc522_tag_t*) data->ptr;
+                ESP_LOGI(TAG, "Tag scanned (sn: %" PRIu64 ")", tag->serial_number);
+            }
+            break;
     }
+}
+
+void app_main()
+{
+    rc522_config_t config = {
+        .spi.host = VSPI_HOST,
+        .spi.miso_gpio = 19,
+        .spi.mosi_gpio = 23,
+        .spi.sck_gpio = 18,
+        .spi.sda_gpio = 5,
+    };
+
+    rc522_create(&config, &scanner);
+    rc522_register_events(scanner, RC522_EVENT_ANY, rc522_handler, NULL);
+    rc522_start(scanner);
 }
