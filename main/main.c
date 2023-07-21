90202f488f88de6bd7622fea0e87aad1dff086f5
mooujarrar <mohyiddine1997@gmail.com>
2023-07-21 19:12:02 +0200

    maint; integration of the all newly implemented stories into the main

--- main/main.c
+++ main/main.c
@@ -5,6 +5,7 @@
 #include "webapp.h"
 #include "access-point.h"
 #include "database.h"
+#define MDNS_INSTANCE "esp home web server"
 
 static const char* TAG = "rfid-web";
 static rc522_handle_t scanner;
@@ -30,18 +31,64 @@ static void disconnect_handler(void* arg, esp_event_base_t event_base,
                                int32_t event_id, void* event_data)
 {
     httpd_handle_t* server = (httpd_handle_t*) arg;
-    webapp_stop_webserver(server);   
+    if (*server != NULL) {
+        webapp_stop_webserver();
+    }
 }
 
 static void connect_handler(void* arg, esp_event_base_t event_base,
                             int32_t event_id, void* event_data)
 {
     httpd_handle_t* server = (httpd_handle_t*) arg;
-
     if (*server == NULL) {
-        const webapp_handle_t webapp = webapp_get_defaults();
-        *server = webapp_start_webserver(webapp);
+        webapp_start_webserver();
     }
+}
+
+static void initialise_mdns(void)
+{
+    mdns_init();
+    mdns_hostname_set(CONFIG_EXAMPLE_MDNS_HOST_NAME);
+    mdns_instance_name_set(MDNS_INSTANCE);
+
+    mdns_txt_item_t serviceTxtData[] = {
+        {"board", "esp32"},
+        {"path", "/"}
+    };
+
+    ESP_ERROR_CHECK(mdns_service_add("ESP32-WebServer", "_http", "_tcp", 80, serviceTxtData,
+                                     sizeof(serviceTxtData) / sizeof(serviceTxtData[0])));
+}
+
+esp_err_t init_fs(void)
+{
+    esp_vfs_spiffs_conf_t conf = {
+        .base_path = CONFIG_EXAMPLE_WEB_MOUNT_POINT,
+        .partition_label = NULL,
+        .max_files = 20,
+        .format_if_mount_failed = false
+    };
+    esp_err_t ret = esp_vfs_spiffs_register(&conf);
+
+    if (ret != ESP_OK) {
+        if (ret == ESP_FAIL) {
+            ESP_LOGE(TAG, "Failed to mount or format filesystem");
+        } else if (ret == ESP_ERR_NOT_FOUND) {
+            ESP_LOGE(TAG, "Failed to find SPIFFS partition");
+        } else {
+            ESP_LOGE(TAG, "Failed to initialize SPIFFS (%s)", esp_err_to_name(ret));
+        }
+        return ESP_FAIL;
+    }
+
+    size_t total = 0, used = 0;
+    ret = esp_spiffs_info(NULL, &total, &used);
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to get SPIFFS partition information (%s)", esp_err_to_name(ret));
+    } else {
+        ESP_LOGI(TAG, "Partition size: total: %d, used: %d", total, used);
+    }
+    return ESP_OK;
 }
 
 void app_main()
@@ -58,9 +105,14 @@ void app_main()
     ESP_ERROR_CHECK(ret);
 
     /*
+        Initialize the FS: SSPI flash to host the webapp
+    */
+    ESP_ERROR_CHECK(init_fs());
+
+    /*
         Initialize WIFI SoftAP
     */ 
-    static httpd_handle_t server = NULL;
+    extern httpd_handle_t server;
     ESP_LOGI(TAG, "START WiFi SOFT AP");
     wifi_init_softap();
     // In case of connecting to the SoftAP, we launch the webserver
@@ -86,6 +138,14 @@ void app_main()
         .spi.sda_gpio = CONFIG_ESP_RFID_SDA,
     };
     ESP_LOGI(TAG, "START RFID SCANNER");
+
+    /*
+        Initialize mdns to give to the ESP32 a Domain name, default: esp-home.local
+    */
+    initialise_mdns();
+    netbiosns_init();
+    netbiosns_set_name(CONFIG_EXAMPLE_MDNS_HOST_NAME);
+
     rc522_create(&config, &scanner);
     rc522_register_events(scanner, RC522_EVENT_ANY, rc522_handler, NULL);
     rc522_start(scanner);
